58e1ff022595343fcbf6136e70c5f127241be643
Tumaini Muriithi <wendomuriithi@gmail.com>
2024-03-10 20:09:37 +0300

    Made a lot of changes

---
+++ src/components/Review_Table.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import {Container, Table, Button} from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+
+function Review_Table() {
+  const  [reviews, setReviews] = useState([])
+    useEffect(()=>{
+        async function fetchReviewData(){
+            try{
+                await fetch('/reviews')
+                .then(response => response.json())
+                .then(data => setReviews(data))
+            }catch (error){
+                console.log('Error: ', error)
+            }
+        }
+        fetchReviewData()
+    },[])
+    // console.log(reviews)
+
+    function handleDelete(id) {
+        fetch(`/reviews/${id}`, {
+            method: "DELETE",
+            }).then((r) => {
+            if (r.ok) {
+                setReviews((reviews) =>
+                reviews.filter((review) => review.id !== id)
+                );
+            }
+            });
+
+            alert ("Deleted")
+
+      } 
+    
+  return (
+    <div>
+      <Container className='table-container'>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Desccription</th>
+                    <th>Rating</th>
+                    <th>Created at:</th>
+                    <th>Product Name</th>
+                    <th>User name</th>
+                    <th></th>
+                    <th>Delete</th>
+                    <th>Update</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {reviews.map(item => (
+                        <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.description}</td>
+                        <td>{item.rating}</td>
+                        <td>{item.product.name}</td>
+                        <td>{item.user.username}</td>
+                        <td><Button variant='danger' onClick={() => handleDelete(item.id)}>Delete</Button></td>
+                        <td><Button variant='success'><Link to={`/reviews/${item.id}/edit`} className="link">Update Review</Link></Button></td> 
+                        {/* Maybe we can update the review? */}
+                    </tr>
+                    ))}
+                </tbody>
+            </Table>
+        </Container>
+    </div>
+  )
+}
+
+export default Review_Table
---
+++ src/components/Reviews_Form.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import { useState, useEffect} from "react"
+import {Form,Row,Col,Container, Button} from 'react-bootstrap';
+
+function Reviews_Form() {
+
+  const[loading, setLoading]= useState(false);
+//   const[error, setError]= useState(null);
+  const [usernameID, setUsernameID] = useState("");
+  const [productID, setProductID] = useState("");
+  const[users, setUsers]= useState([])
+  const [products, setproducts]= useState([])
+  const [description, setDescription]=useState('')
+  const [rating, setRating]=useState('')
+
+  
+    useEffect(() => {
+        fetch("/users")
+            .then((r) => r.json())
+            .then(setUsers);
+        }, []);
+
+    useEffect(() => {
+        fetch("/products")
+            .then((r) => r.json())
+            .then(setproducts);
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = {
+            username_id: parseInt(usernameID),
+            product_id: parseInt(productID),
+            description,
+            rating,
+        };
+        try {
+            const response = await fetch('/appointments', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            setLoading(false);
+        } catch (error) {
+            // Handle error
+            setLoading(false);
+        } finally {
+            window.location.reload();
+        }
+    };
+
+  if  (loading){
+    return <h4>Loading...</h4>
+  }
+
+  return (
+    <div>
+        <Container className='mb-5'>
+            <h3 className="text-center mt-3 mb-4">Book Appointment</h3>
+            <Form onSubmit={handleSubmit}>
+                <Row>
+                    <Col>
+                        <Form.Control name='description' value={description} onChange={(e) => setDescription(e.target.value)} type='text' placeholder="Description" /><br/>
+                        <Form.Select name='rating' value={rating} onChange={(e) => setRating(e.target.value)} aria-label="Default select example">
+                            <option>Rating</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </Form.Select><br/>
+                    </Col>
+                    <Col>
+                        <Form.Select name='product_id' value={productID} onChange={(e) => setProductID(e.target.value)} aria-label="Default select example">
+                        <option value="">Select product</option>
+                            {products.map((product) => (
+                            <option key={product.id} value={product.id}>
+                                {product.name}
+                            </option>
+                            ))}
+                        </Form.Select><br/>    
+                        <Form.Select name='username_id' value={usernameID} onChange={(e) => setUsernameID(e.target.value)} aria-label="Default select example">
+                        <option value="">Select your username</option>
+                            {users.map((user) => (
+                            <option key={user.id} value={user.id}>
+                                {user.username}
+                            </option>
+                            ))}
+                        </Form.Select><br/>   
+                    </Col>
+                </Row>
+                <Button variant="primary" type="submit">
+                    Create Review
+                </Button>
+            </Form>
+        </Container>
+    </div>
+  )
+}
+
+export default Reviews_Form
