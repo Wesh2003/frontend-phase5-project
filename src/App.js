fe30267f14862dd36a6e624946e59c637f553939
evans <evansnt9619@gmail.com>
2024-03-18 12:06:14 +0300

    images

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   const handleLogin = (userId) => {
     setIsAuthenticated(true);
     setUserId(userId); // Store the user ID
-    Redirect('/userprofile');
+    history.push('/userprofile');
   };
  
 
