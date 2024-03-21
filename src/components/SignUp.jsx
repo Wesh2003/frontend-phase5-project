bf5b7d80a27bda6508a1b2d9aa9ca049c4db4305
Tumaini Muriithi <wendomuriithi@gmail.com>
2024-03-22 01:29:27 +0300

    Fixed receipt and reviews to work

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -32,7 +32,7 @@ function SignUp() {
       console.log(response);
       if (response.ok) {
         // Redirect to products page upon successful signup
-        window.location.replace('/home'); 
+        window.location.replace('/login'); 
       } else {
         // Handle signup error
         console.error('Signup failed');
