857af179d31b495962f9a261db8856da6ac6a7e0
Tumaini Muriithi <wendomuriithi@gmail.com>
2024-03-22 12:02:24 +0300

    Fixed total cost

--- src/components/ShoppingCartTable.jsx
+++ src/components/ShoppingCartTable.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { Link } from "react-router-dom";
 
 
